53d39f2395a69329d57e0678369dde49a7f081f7
novadiscord-development <dylanlayton098@gmail.com>
2024-07-03 19:39:02 +0000

    node .

---
+++ api/vote.js
@@ -0,0 +1,36 @@
+import voteSchema from "@/schemas/vote.schema";
+import mongoConnect from "@/util/mongo-connect";
+
+const TOPGG_SECRET = "5f1a4826-41ae-401b-bef3-07ed96b4a65b";
+
+const POST = async (req, res) => {
+   const { user: userId } = req.body;
+
+   await mongoConnect();
+
+   await new voteSchema({
+      _id: userId,
+   }).save();
+
+   res.status(200).json({ message: `Vote has been recorded for ${userId}.` });
+};
+
+const BumpInfo = async (req, res) => {
+   const { authorization } = req.headers;
+
+   if (!authorization) {
+      return res.status(401).json({ error: "Unauthorized" });
+   }
+
+   if (authorization !== TOPGG_SECRET) {
+      return res.status(401).json({ error: "Unauthorized" });
+   }
+
+   if (req.method === "POST") {
+      await POST(req, res);
+   } else {
+      res.status(405).end();
+   }
+};
+
+export default BumpInfo;
